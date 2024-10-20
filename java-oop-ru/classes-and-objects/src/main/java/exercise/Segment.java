e5cf06d19471f4016c1137fce44223a57043acb2
@hexlet/cli <support@hexlet.io>
2024-10-21 01:40:30 +0500

    submit java-oop-ru/classes-and-objects

--- java-oop-ru/classes-and-objects/src/main/java/exercise/Segment.java
+++ java-oop-ru/classes-and-objects/src/main/java/exercise/Segment.java
@@ -21,21 +21,5 @@ package exercise;
         int midPoint2 = (this.x.getY() + this.y.getY())/2;
         return new Point(midPoint1, midPoint2);
     }
-
 }
-
-/*
-Создайте класс Segment. Внутри класса реализуйте конструктор, который принимает две точки — начало и конец отрезка.
-Точка — это объект класса Point. В классе определите следующие методы:
-
-getBeginPoint() — возвращает начальную точку отрезка.
-getEndPoint() — возвращает конечную точку отрезка.
-getMidPoint() — возвращает новую точку — середину отрезка.
-Point point1 = new Point(4, 3);
-Point point2 = new Point(6, 1);
-Segment segment = new Segment(point1, point2);
-Point midPoint = segment.getMidPoint();
-midPoint.getX(); // 5
-midPoint.getY(); // 2
- */
 // END
